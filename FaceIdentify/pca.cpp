105379004d3bca4a727ddbeb86b1ce85e8a1ca85
姜海强 <haiqiangjiang@deepglint.com>
2019-04-29 23:15:50 +0800

    pca done

--- FaceIdentify/pca.cpp
+++ FaceIdentify/pca.cpp
@@ -33,13 +33,13 @@ int main(int argc, char** argv)
 		drawContours(result, contours, i, Scalar(0, 0, 255), 2, 8);
 		double theta = calcPCAOrientation(contours[i], result);
 	}
-
+	imshow("contours", result);
 
 	waitKey(0);
 	return 0;
 }
 
-double calcPCAOrientation(vector<Point> &pts, Mat &CALIB_CB_NORMALIZE_IMAGE)
+double calcPCAOrientation(vector<Point> &pts, Mat &image)
 {
 	int size = static_cast<int>(pts.size());
 	Mat data_pts = Mat(size, 2, CV_64FC1);
@@ -49,4 +49,26 @@ double calcPCAOrientation(vector<Point> &pts, Mat &CALIB_CB_NORMALIZE_IMAGE)
 		data_pts.at<double>(i, 1) = pts[i].y;
 	}
 	PCA pca_analysis(data_pts, Mat(), CV_PCA_DATA_AS_ROW);
+	Point cnt = Point(static_cast<int>(pca_analysis.mean.at<double>(0, 0)),
+		   static_cast<int>(pca_analysis.mean.at<double>(0,1)));
+
+	circle(image, cnt, 2, Scalar(0, 255, 0), 2, 8, 0);
+
+	vector<Point2d> vecs(2);
+	vector<double>	vals(2);
+	for (int i = 0; i < 2; i++)
+	{
+		vals[i] = pca_analysis.eigenvalues.at<double>(i, 0);
+		cout << "eigen value" << i << vals[i] << endl;
+		vecs[i] = Point2d(pca_analysis.eigenvectors.at<double>(i, 0), pca_analysis.eigenvectors.at<double>(i, 1));
+	}
+	Point p1 = cnt + 0.02*Point(static_cast<int>(vecs[0].x*vals[0]), static_cast<int>(vecs[0].y*vals[0]));
+	Point p2 = cnt + 0.02*Point(static_cast<int>(vecs[1].x*vals[1]), static_cast<int>(vecs[1].y*vals[1]));
+
+	line(image, cnt, p1, Scalar(255, 0, 0), 2, 8, 0);
+	line(image, cnt, p2, Scalar(255, 255, 0), 2, 8, 0);
+
+	double angle = atan2(vecs[0].y, vecs[0].x);
+	return angle;
+
 }
